8698a57862d855086e10faece5caab41a1975910
Devwarlt <devwarlt@loesoft.org>
2020-11-05 15:16:09 -0200

    First module, just basics nothing else

---
+++ CppFTROU.Module1/GreenBottles.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	int greenBottles = 10;
+	std::cout << "There were " << greenBottles-- << " green bottles." << std::endl;
+	std::cout << "There were " << greenBottles << " green bottles." << std::endl;
+}
