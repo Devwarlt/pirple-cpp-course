e87542f7c3e5d620e1fe220d66aec54bc58cdeac
Devwarlt <devwarlt@loesoft.org>
2020-11-06 00:48:15 -0200

    Update GreenBottles.cpp

--- CppFTROU.Module1/GreenBottles.cpp
+++ CppFTROU.Module1/GreenBottles.cpp
@@ -5,4 +5,6 @@ int main()
 	int greenBottles = 10;
 	std::cout << "There were " << greenBottles-- << " green bottles." << std::endl;
 	std::cout << "There were " << greenBottles << " green bottles." << std::endl;
+
+	return EXIT_SUCCESS;
 }
