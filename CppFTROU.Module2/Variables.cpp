bc2c5dce21cac0cd1543fa47e22c0489241ec927
Devwarlt <devwarlt@loesoft.org>
2020-11-06 01:05:44 -0200

    Second module: rand + numeric ops + const declarations

---
+++ CppFTROU.Module2/Variables.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <ctime>
+#include <random>
+
+const unsigned int MINUTES_PER_HOUR = 60;
+const unsigned int MAX_HOURS_RANGE = 24;
+
+int main()
+{
+	std::srand(time(0));
+
+	auto numHours = (std::rand() % MAX_HOURS_RANGE) + 1;
+
+	// MINUTES_PER_HOUR++;
+	numHours++;
+
+	auto numMins = numHours * MINUTES_PER_HOUR;
+
+	std::cout << "There are " << numMins << " minutes in " << numHours << " hours." << std::endl;
+
+	unsigned int zeroHour = 0;
+	zeroHour--;
+
+	std::cout << "1 hour before zero hour is " << zeroHour << std::endl;
+
+	return EXIT_SUCCESS;
+}
