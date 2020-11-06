cf9ab3d4305b96537e3ac609db2c7c5904a574a4
Devwarlt <devwarlt@loesoft.org>
2020-11-06 15:25:13 -0200

    Third module: I/O ops + console formatting

---
+++ CppFTROU.Module3/Prices.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+	float price1 = 0.0F,
+		price2 = 0.0F,
+		price3 = 0.0F,
+		total = 0.0F;
+
+	std::cout << "Enter a price:" << std::endl;
+	std::cin >> price1;
+
+	std::cout << "Enter a price:" << std::endl;
+	std::cin >> price2;
+
+	std::cout << "Enter a price:" << std::endl;
+	std::cin >> price3;
+
+	total = price1 + price2 + price3;
+
+	std::cout
+		<< std::setiosflags(std::ios::fixed) << std::setprecision(2)
+		<< std::endl
+		<< "Price 1 is" << std::setw(9) << price1 << std::endl
+		<< "Price 2 is" << std::setw(9) << price2 << std::endl
+		<< "Price 3 is" << std::setw(9) << price3 << std::endl
+		<< std::endl
+		<< "Total price: " << std::setw(9) << total << std::endl;
+
+	return EXIT_SUCCESS;
+}
